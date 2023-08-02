681b6f999ee93cef79a47b844ea29cba3cfb299a
liliperezglez <lilianaperezgonzlez@gmail.com>
2023-08-02 13:18:44 +0200

    añadir avatar a user

---
+++ src/controllers/entries/addAvatar.js
@@ -0,0 +1,39 @@
+const getDB = require("../../database/db");
+const saveAvatar = require("../../service/saveAvatar");
+const fs = require("fs/promises");
+const path = require("path");
+
+const addAvatar = async (req, res) => {
+  try {
+    const connect = await getDB();
+    const { idUser } = req.params;
+
+    const avatarFolder = path.resolve(__dirname, "../../uploads/avatarUser");
+
+    const createFolderUser = async () => {
+      try {
+        await fs.access(`${avatarFolder}/${idUser}`);
+      } catch (error) {
+        await fs.mkdir(`${avatarFolder}/${idUser}`);
+      }
+    };
+
+    if (req.files && req.files.avatar) {
+      createFolderUser();
+      const avatar = await saveAvatar(req.files.avatar, `/avatarUser/${idUser}`);
+
+      return res.status(200).send({
+        status: "OK",
+        message: "El avatar se cargó correctamente",
+        avatar,
+      });
+    } else {
+      return res.status(409).send("El envío del avatar es obligatorio");
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Error al agregar el avatar");
+  }
+};
+
+module.exports = addAvatar;
--- src/controllers/entries/index.js
+++ src/controllers/entries/index.js
@@ -1,4 +1,5 @@
 const addPhoto = require("./addPhoto");
+const addAvatar = require("./addAvatar");
 const searchPhotoDescr = require("./searchPhotoDescr");
 
 const likeEntry = require("./likeEntry");
@@ -6,7 +7,8 @@ const deleteEntry = require('./deleteEntry');
 
 module.exports = {
   addPhoto,
+  addAvatar,
   searchPhotoDescr,
   likeEntry,
-  deleteEntry
+  deleteEntry,
 };
