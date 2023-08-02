fdffeb42299c6d8157b2e4dd5944278fa231343b
Juan esteban <esteban66628@gmail.com>
2023-08-02 20:42:26 +0200

    All fotos con likes añadido

--- src/controllers/entries/seePhotoUsers.js
+++ src/controllers/entries/seePhotoUsers.js
@@ -1,13 +1,12 @@
 const getDB = require("../../database/db");
-const fs = require("fs/promises");
-const path = require("path");
 
 const seePhotoUsers = async (req, res) => {
   try {
     const connect = await getDB();
     const [users] = await connect.query(
       `
-                SELECT  u.name, u.email, p.photo, p.date, e.description, e.comment, e.place
+                SELECT  u.name, u.email as userName, p.photo, 
+                p.date, e.description, e.place, e.id as idEntry
                 FROM users u
                  JOIN entries e ON e.user_id=u.id
                  JOIN photos p ON p.entry_id=e.id
@@ -16,16 +15,39 @@ const seePhotoUsers = async (req, res) => {
     );
 
     let userMap = users.map((user) => {
-      return [user.email, user];
+      return [user.userName, user];
     });
     const userMapArr = new Map(userMap);
-    const userUnique = [...userMapArr.values()];
+    let userUnique = [...userMapArr.values()];
 
     userUnique.sort(function (a, b) {
       return b.date - a.date;
     });
 
-    res.send(userUnique);
+    const PhotosWithLikes = userUnique.map(async (user) => {
+      const [total] = await connect.query(
+        `
+      SELECT  COUNT(*) as likes, e.id as idEntry
+      FROM entries e
+      JOIN likes l ON l.entry_id = e.id
+      WHERE entry_id=?
+    `,
+        [user.idEntry]
+      );
+      if (user.idEntry === total[0].idEntry) {
+        user["likes"] = total[0].likes;
+      } else {
+        user["likes"] = 0;
+      }
+    });
+
+    Promise.all(PhotosWithLikes).then(async () => {
+      await res.status(200).send({
+        status: "OK",
+        data: userUnique,
+      });
+    });
+
     connect.release();
   } catch (error) {
     console.log(error);
--- src/controllers/users/seeUserProfile.js
+++ src/controllers/users/seeUserProfile.js
@@ -1,45 +1,64 @@
 const getDB = require("../../database/db");
 
 const seeUserProfile = async (req, res) => {
-    try {
-        const { idUser } = req.params;
-        const connect = await getDB();
-    
-        const [user] = await connect.query(
-          `SELECT date, email, name, avatar FROM users WHERE id=?`,
-          [idUser]
-        );
+  try {
+    const { idUser } = req.params;
+    const connect = await getDB();
 
-        if (!user.length) {
-            return res.status(404).send({
-              status: 'Not Found',
-              message: 'User not found',
-            });
-          }
+    const [user] = await connect.query(
+      `SELECT date, email, name, avatar FROM users WHERE id=?`,
+      [idUser]
+    );
 
-          const [photos] = await connect.query(
-            `SELECT date, photo FROM photos WHERE entry_id IN (SELECT id FROM entries WHERE user_id=?)`,
-            [idUser]
-          );
-      
-          connect.release();
+    if (!user.length) {
+      return res.status(404).send({
+        status: "Not Found",
+        message: "User not found",
+      });
+    }
 
-          res.status(200).send({
-            status: 'OK',
-            data: {
-                user: user[0],
-                photos,
-             }
-          });
-      
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({
-           status: 'Error',
-           message: 'Internal Server Error',
+    const [photos] = await connect.query(
+      `SELECT date, photo, entry_id as idEntry 
+      FROM photos 
+      WHERE entry_id IN (SELECT id FROM entries WHERE user_id=?)`,
+      [idUser]
+    );
+
+    const PhotosWithLikes = photos.map(async (user) => {
+      const [total] = await connect.query(
+        `
+      SELECT  COUNT(*) as likes, e.id as idEntry
+      FROM entries e
+      JOIN likes l ON l.entry_id = e.id
+      WHERE entry_id=?
+    `,
+        [user.idEntry]
+      );
+      if (user.idEntry === total[0].idEntry) {
+        user["likes"] = total[0].likes;
+      } else {
+        user["likes"] = 0;
+      }
     });
 
-    }
+    Promise.all(PhotosWithLikes).then(async () => {
+      await res.status(200).send({
+        status: "OK",
+        data: {
+          user: user[0],
+          photos: photos,
+        },
+      });
+    });
+
+    connect.release();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      status: "Error",
+      message: "Internal Server Error",
+    });
+  }
 };
 
 module.exports = seeUserProfile;
