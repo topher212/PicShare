df129b8f24106fb7d987a8c5f7c6f14910789ab8
liliperezglez <lilianaperezgonzlez@gmail.com>
2023-08-04 16:41:56 +0200

    Editar perfil de usuario

---
+++ src/middlewares/canEditUser.js
@@ -0,0 +1,31 @@
+const getDB = require('../database/db');
+
+const canEditUser = async (req, res, next) => {
+    try {
+        const connect = await getDB();
+        const { idUser } = req.params;
+
+        const [user] = await connect.query(
+            `SELECT id
+             FROM users
+             WHERE id = ?`, [idUser]
+        );
+
+        connect.release();
+
+        if (req.userInfo.id !== user[0].id) {
+            return res.status(401).send('No tienes permisos para modificar este comentario');
+        };
+
+        next();
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            status: "ERROR",
+            message: "Error en el servidor",
+          });
+    }
+};
+
+module.exports = canEditUser;
