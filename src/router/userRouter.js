37f6d9c0a487a926a07eeb7172295de7bd56f638
dgr92 <david-gr_92@hotmail.com>
2023-08-04 19:27:15 +0200

    arreglada actualización de avatar

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -7,7 +7,7 @@ const isUser = require("../middlewares/isUser");
 const validatorFiles = require("../middlewares/validatorFiles");
 const validatorInfoUser = require("../middlewares/validatorInfoUser");
 const canDeleteUser = require("../middlewares/canDeleteUser");
-const avatarExists = require("../middlewares/avatarExists");
+const deleteOldAvatar = require("../middlewares/deleteOldAvatar");
 const canEditUser = require("../middlewares/canEditUser");
 
 const {
@@ -15,28 +15,15 @@ const {
   deleteUser,
   loginUser,
   seeUserProfile,
-  addAndChangeAvatar,
+  addNewAvatar,
   editUser,
 } = require("../controllers/users");
 
 router.post("/register", validatorInfoUser, postUser);
 router.post("/login", loginUser);
-router.post(
-  "/users/avatar",
-  isUser,
-  validatorFiles,
-  avatarExists,
-  addAndChangeAvatar
-);
+router.post("/users/avatar", isUser, validatorFiles, deleteOldAvatar, addNewAvatar);
 router.get("/users/:idUser", userExists, seeUserProfile);
 router.delete("/users/:idUser", isUser, canDeleteUser, deleteUser);
-router.put(
-  "/users/:idUser",
-  isUser,
-  userExists,
-  canEditUser,
-  validatorInfoUser,
-  editUser
-);
+router.put("/users/:idUser", isUser, userExists, canEditUser, validatorInfoUser, editUser);
 
 module.exports = router;
