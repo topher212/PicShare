39bf3e2ff7e1e7376ad76ad0ab0dc748f8f44040
dgr92 <david-gr_92@hotmail.com>
2023-10-08 15:10:39 +0200

    solucionado error

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const PORT = process.env.PORT || 3001; 
-const USER = process.env.PORT || 'demo'; 
-const PASSWORD = process.env.PORT ||'password'; 
-const HOST = process.env.PORT || 'localhost'; 
-const DATABASE = process.env.PORT || 'picshare'; 
-const PORT_DB = process.env.PORT || '3306'; 
+const USER = process.env.USER || 'demo'; 
+const PASSWORD = process.env.PASSWORD ||'password'; 
+const HOST = process.env.HOST || 'localhost'; 
+const DATABASE = process.env.DATABASE || 'picshare'; 
+const PORT_DB = process.env.PORT_DB || '3306'; 
 
 module.exports = {PORT, USER, PASSWORD, HOST, DATABASE, PORT_DB};
