43d341745bdad28ca42ce3e96d215baa3410ecc6
dgr92 <david-gr_92@hotmail.com>
2023-10-09 13:42:10 +0200

    añadido ping a la base de datos para evitar el cierre de la conexion

--- src/database/db.js
+++ src/database/db.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql2/promise");
-const {USER, PASSWORD, HOST, DATABASE, PORT_DB} = require('../config');
+const { USER, PASSWORD, HOST, DATABASE, PORT_DB } = require('../config');
 require("dotenv").config();
 
 let pool;
@@ -17,5 +17,20 @@ const getDB = async () => {
   }
   return await pool.getConnection();
 };
+
+const pingDatabase = async () => {
+  try {
+    const connect = await getDB();
+    const [results, fields] = await connect.query('SELECT 1')
+    connect.release();
+
+  } catch (error) {
+    console.error('Error al hacer ping a la base de datos:', error);
+  }
+}
+
+// Programa una tarea que ejecute pingDatabase cada 4 horas (en milisegundos)
+const fourHours = 4 * 60 * 60 * 1000;
+setInterval(pingDatabase, fourHours);
 
 module.exports = getDB;
